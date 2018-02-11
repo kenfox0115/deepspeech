a023ce3f798e1f580529baa2745a867e0e0168b3
kenfox0115 <ken.c.fox@gmail.com>
2018-02-11 11:22:10 -0800

    removed comments

--- deepspeech_setup.sh
+++ deepspeech_setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#apt
 apt-get update
 apt-get upgrade
 
-#git
+
 apt-get install software-properties-common -y
 add-apt-repository ppa:git-core/ppa -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 apt-get install git-lfs -y
 git lfs install
 
-#DeepSpeech
+
 git clone https://github.com/mozilla/DeepSpeech
 cd Deepspeech
 git lfs fetch
