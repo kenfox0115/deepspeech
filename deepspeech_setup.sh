608fccd3281e0c1efb313f1dccabc4882228cd6d
kenfox0115 <ken.c.fox@gmail.com>
2018-02-11 10:53:53 -0800

    script for setting up deepspeech environment

---
+++ deepspeech_setup.sh
@@ -0,0 +1,22 @@
+cd /
+
+#apt
+sudo apt-get update
+sudo apt-get upgrade
+
+#git
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:git-core/ppa -y
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs -y
+git lfs install
+
+#DeepSpeech
+sudo git clone https://github.com/mozilla/DeepSpeech
+cd Deepspeech
+git lfs fetch
+git lfs checkout
+sudo apt-get install python-pip -y
+sudo pip install tensorflow
+sudo pip install deepspeech
+python util/taskcluster.py --target .
