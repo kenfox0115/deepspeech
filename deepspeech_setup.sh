4489a544147c70e50908a4e031ad5f681796e858
kenfox0115 <ken.c.fox@gmail.com>
2018-02-11 11:02:53 -0800

    removed sudos

--- deepspeech_setup.sh
+++ deepspeech_setup.sh
@@ -1,20 +1,20 @@
 #apt
-sudo apt-get update
-sudo apt-get upgrade
+apt-get update
+apt-get upgrade
 
 #git
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:git-core/ppa -y
+apt-get install software-properties-common -y
+add-apt-repository ppa:git-core/ppa -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs -y
+apt-get install git-lfs -y
 git lfs install
 
 #DeepSpeech
-sudo git clone https://github.com/mozilla/DeepSpeech
+git clone https://github.com/mozilla/DeepSpeech
 cd Deepspeech
 git lfs fetch
 git lfs checkout
-sudo apt-get install python-pip -y
-sudo pip install tensorflow
-sudo pip install deepspeech
+apt-get install python-pip -y
+pip install tensorflow
+pip install deepspeech
 python util/taskcluster.py --target .
