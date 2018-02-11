8b100c0ab0b5615e2a58c3ce8218527b814f3bd3
kenfox0115 <ken.c.fox@gmail.com>
2018-02-11 12:07:51 -0800

    added apt-get curl

--- deepspeech_setup.sh
+++ deepspeech_setup.sh
@@ -8,6 +8,7 @@ apt-get upgrade -y
 
 apt-get install software-properties-common -y
 add-apt-repository ppa:git-core/ppa -y
+apt-get install curl -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 apt-get install git-lfs -y
 git lfs install
