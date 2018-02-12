0d484e8f2f814b279a190716b78e11b65d69b243
kenfox0115 <ken.c.fox@gmail.com>
2018-02-12 14:25:44 -0800

    removed tensorflow install -covered by reqs

--- setup_all_DS.sh
+++ setup_all_DS.sh
@@ -21,7 +21,6 @@ git lfs checkout
 apt-get install python-pip -y
 pip install --upgrade pip
 pip install -r /DeepSpeech/requirements.txt
-pip install tensorflow
 pip install deepspeech
 cd /DeepSpeech/native_client
 python /DeepSpeech/util/taskcluster.py --target .
