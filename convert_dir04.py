7a06b51e3b9cc2fc7c39517c7ebd8f6ba1ba7afe
kenfox0115 <ken.c.fox@gmail.com>
2018-02-16 09:00:28 -0800

    added ffmpy funtionality to convert_dir04.py. still failing on overwrite due to same name

--- convert_dir04.py
+++ convert_dir04.py
@@ -1,11 +1,18 @@
 import os
+import ffmpy
+
 directory = '/mnt/hgfs/shared'
 
 
 for filename in os.listdir('/mnt/hgfs/shared'):
-    if filename.endswith(".mov") or filename.endswith(".py"): 
+    if filename.endswith(".mov") or filename.endswith(".mp4"): 
         file_name = os.path.join(directory, filename)
 	print("Converting "+file_name)
-        continue
+	ff = ffmpy.FFmpeg(
+		inputs={file_name: None},
+		outputs={'output.avi': None}
+	)
+	ff.run()
+	continue
     else:
         continue
