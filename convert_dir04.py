7bc87474fc46b5b0397fbb6f1b1c99bb6cdd14bb
kenfox0115 <ken.c.fox@gmail.com>
2018-02-16 08:42:18 -0800

    made file_name a variable in convert_dir04.py

--- convert_dir04.py
+++ convert_dir04.py
@@ -4,7 +4,8 @@ directory = '/mnt/hgfs/shared'
 
 for filename in os.listdir('/mnt/hgfs/shared'):
     if filename.endswith(".mov") or filename.endswith(".py"): 
-        print("Converting "+os.path.join(directory, filename))
+        file_name = os.path.join(directory, filename)
+	print("Converting "+file_name)
         continue
     else:
         continue
