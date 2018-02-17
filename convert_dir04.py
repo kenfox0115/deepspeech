981921b09937164b1652913e199ef747b34b3b5b
kenfox0115 <ken.c.fox@gmail.com>
2018-02-16 17:19:12 -0800

    got filename only reporting working

--- convert_dir04.py
+++ convert_dir04.py
@@ -7,12 +7,14 @@ directory = '/mnt/hgfs/shared'
 for filename in os.listdir('/mnt/hgfs/shared'):
     if filename.endswith(".mov") or filename.endswith(".mp4"): 
         file_name = os.path.join(directory, filename)
-	print("Converting "+file_name)
-	ff = ffmpy.FFmpeg(
-		inputs={file_name: None},
-		outputs={'output.avi': None}
-	)
-	ff.run()
+	print(os.path.basename(file_name).split('.')[0])
+	#stripped_name = os.path.splitext(flie_name)[0]))	
+	#print("Converting "+stripped_name)
+	#ff = ffmpy.FFmpeg(
+	#	inputs={file_name: None},
+	#	outputs={'output.avi': None}
+	#)
+	#ff.run()
 	continue
     else:
         continue
