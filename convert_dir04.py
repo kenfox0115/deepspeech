4261f6598db2945b9ca2aaddc22ae286688501bc
kenfox0115 <ken.c.fox@gmail.com>
2018-02-16 18:09:31 -0800

    convert_dir04.py now puts output files in same dir as original files

--- convert_dir04.py
+++ convert_dir04.py
@@ -1,5 +1,4 @@
 import os
-import ffmpy
 
 directory = '/mnt/hgfs/shared'
 
@@ -8,15 +7,10 @@ for filename in os.listdir('/mnt/hgfs/shared'):
     if filename.endswith(".mov") or filename.endswith(".mp4"): 
         file_name = os.path.join(directory, filename)
 	stripped_name = os.path.basename(file_name).split('.')[0]
-	output_name = stripped_name+".wav"
+	output_name = directory+"/"+stripped_name+".wav"
 	print("Converting "+filename)
 	print("to "+output_name)
 	os.system('ffmpeg -i %s -ar 16000 -ac 1 %s'%(file_name, output_name))
-	#ff = ffmpy.FFmpeg(
-	#	inputs={file_name: None},
-	#	outputs={output_name: '-ar 16000, -ac 1'}
-	#)
-	#ff.run()
 	continue
     else:
         continue
