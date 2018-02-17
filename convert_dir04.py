92b246f233c90a7511c21c9a6ecd921f4917acdb
kenfox0115 <ken.c.fox@gmail.com>
2018-02-16 18:04:36 -0800

    convert_dir04.py is now fully working (!)

--- convert_dir04.py
+++ convert_dir04.py
@@ -7,12 +7,14 @@ directory = '/mnt/hgfs/shared'
 for filename in os.listdir('/mnt/hgfs/shared'):
     if filename.endswith(".mov") or filename.endswith(".mp4"): 
         file_name = os.path.join(directory, filename)
-	print(os.path.basename(file_name).split('.')[0])
-	#stripped_name = os.path.splitext(flie_name)[0]))	
-	#print("Converting "+stripped_name)
+	stripped_name = os.path.basename(file_name).split('.')[0]
+	output_name = stripped_name+".wav"
+	print("Converting "+filename)
+	print("to "+output_name)
+	os.system('ffmpeg -i %s -ar 16000 -ac 1 %s'%(file_name, output_name))
 	#ff = ffmpy.FFmpeg(
 	#	inputs={file_name: None},
-	#	outputs={'output.avi': None}
+	#	outputs={output_name: '-ar 16000, -ac 1'}
 	#)
 	#ff.run()
 	continue
