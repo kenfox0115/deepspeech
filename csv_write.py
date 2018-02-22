a40b1b3e88eb582e4607ad6fe17603b14cf15063
kenfox0115 <ken.c.fox@gmail.com>
2018-02-22 08:23:57 -0800

    making sure that csv_populate.py is synched

--- csv_write.py
+++ csv_write.py
@@ -3,12 +3,16 @@ import os
 
 directory = ('/home/ken/deepspeech')
 outfile = open('output.csv', 'wb')
-writer = csv.writer(outfile)
+writer = csv.writer(outfile, delimiter=',', quotechar='"', escapechar='\\', quoting=csv.QUOTE_NONE)
 
 
 for filename in os.listdir(directory):
 	myfile = os.path.join(directory, filename)
+	#print(myfile)
 	filesize = os.path.getsize(myfile)
+	#print(filesize)
 	str_filesize = str(filesize)
+	#print(str_filesize)
 	writer.writerow([("%s,%s"%(myfile, str_filesize))])
+	#print([("%s,%s"%(myfile, str_filesize))])
 	continue
